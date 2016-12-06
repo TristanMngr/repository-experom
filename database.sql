c67bd45029e5a398ff8a4a996f48bfd66dccf9ca
TristanMngr <tristanmenager@gmail.com>
2016-12-06 23:59:30 +0100

    db remise a niveau

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:8889
--- Généré le :  Dim 04 Décembre 2016 à 21:57
+-- Généré le :  Mar 06 Décembre 2016 à 23:58
 -- Version du serveur :  5.6.28
 -- Version de PHP :  5.6.25
 
@@ -11,57 +11,8 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Base de données :  `exemple`
---
-CREATE DATABASE IF NOT EXISTS `exemple` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `exemple`;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Utilisateurs`
---
-
-CREATE TABLE `Utilisateurs` (
-  `id` int(11) NOT NULL,
-  `identifiant` text COLLATE utf8_unicode_ci NOT NULL,
-  `mdp` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Contenu de la table `Utilisateurs`
---
-
-INSERT INTO `Utilisateurs` (`id`, `identifiant`, `mdp`) VALUES
-(1, 'matthieu', '202cb962ac59075b964b07152d234b70'),
-(2, 'raja', '202cb962ac59075b964b07152d234b70'),
-(3, 'zakia', '202cb962ac59075b964b07152d234b70'),
-(4, 'jeremy', '202cb962ac59075b964b07152d234b70'),
-(5, 'tristan', 'tristan');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `Utilisateurs`
---
-ALTER TABLE `Utilisateurs`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `Utilisateurs`
---
-ALTER TABLE `Utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;--
 -- Base de données :  `experom`
 --
-CREATE DATABASE IF NOT EXISTS `experom` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `experom`;
 
 -- --------------------------------------------------------
 
@@ -76,7 +27,7 @@ CREATE TABLE `users` (
   `adresse` varchar(255) NOT NULL,
   `mdp` varchar(255) NOT NULL,
   `dateInscription` date NOT NULL,
-  `role` int(11) NOT NULL
+  `role` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -84,7 +35,14 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userID`, `nom`, `mail`, `adresse`, `mdp`, `dateInscription`, `role`) VALUES
-(1, 'menager', 'tristanmenager@gmail.com', 'rue des pommes', 'chocolat', '2016-12-04', 0);
+(1, 'menager', 'tristanmenager', 'rue', 'cocos', '2016-12-06', 'principal'),
+(2, 'Marion', 'marion-elodie.langlois@laposte.net', 'rue', 'framboise', '2016-12-06', 'secondaire'),
+(3, 'cocos', 'cocos', 'rue', 'cocos', '2016-12-06', 'secondaire'),
+(4, 'pomme', 'pomme', 'pomme', 'pomme', '2016-12-06', 'principal'),
+(5, 'flop', 'flop@gmail.com', 'flop', 'flop', '2016-12-06', 'principal'),
+(6, 'flopy', 'flopy', 'flop', 'flopy', '2016-12-06', 'secondaire'),
+(7, 'flopflop', 'flopflop', 'flop', 'flopflop', '2016-12-06', 'secondaire'),
+(8, 'trop', 'torp', 'flop', 'trop', '2016-12-06', 'secondaire');
 
 --
 -- Index pour les tables exportées
@@ -104,4 +62,4 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
