85e7c9d32c08612a016c92bc8ecd89f98cf61e99
TristanMngr <tristanmenager@gmail.com>
2016-12-07 20:38:14 +0100

    modif fonction table

--- index.php
+++ index.php
@@ -5,31 +5,36 @@ include("controller/redirection.php");
 ?>
 
 
+<?php
+/*
 
-<!--/*
 
 function select($db, $query, $param) {
     $requete = $db->prepare($query);
-    $requete->execute($param);
+    $requete->execute(array($param));
 
     $donnees = $requete->fetch();
     $requete->closeCursor();
     return $donnees;
 }
 
-function getDansTableUsers($param,$where)
-{
-    if ($where == "userId") {
-        $query = 'SELECT userID, mail, mdp, nom, adresse,role, dateInscription FROM users WHERE userID=:userID';
 
-        $table = $param;
 
+function tableUtilisateur($db,$post,$typeRequete){
+    {
+        if ($typeRequete == "select") {
+            $query = 'SELECT userID, mail, mdp, nom, adresse,role, dateInscription FROM users WHERE userID=:userID';
+        }
+        if ($typeRequete == "update") {
+            $query = 'UPDATE users SET mail=:mail WHERE userID=:userID';
+        }
+        if ($tgypeRequete == "insert"){
+            $query = 'INSERT INTO users(nom, mail, adresse, mdp, dateInscription, role) VALUES(:nom,:mail,:adresse,:mdp,NOW(), :role)';
+        }
+
+        return select($db,$query, ["userID" => $post]);
     }
-    return select($db,$query, ["userID" => $post]);
+
+
 }
-
-
-function tableUtilisateur()
-
-
--->
+*/?>
