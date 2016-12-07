68246ab49c7ad61abb6e9cb4b177919f465aac55
TristanMngr <tristanmenager@gmail.com>
2016-12-07 15:03:24 +0100

    ajout regex mail

--- controller/espaceClient/inscription.php
+++ controller/espaceClient/inscription.php
@@ -16,15 +16,20 @@ if ($utilisateurSecondaire == False) {
         //même principe que pour connexion
         if (!empty($_POST["nom"]) && !empty($_POST["mail"]) && !empty($_POST["adresse"]) && !empty($_POST["mdp"]) && !empty($_POST["rmdp"])) {
             if ($_POST["mdp"] == $_POST["rmdp"]) {
-                if (getDansTableUsers($db, "nom", $_POST["nom"]) == array()) { // on verifie que l'array est vide, permet de verifier que "nom" n'existe pas déja dans la table
-                    insertDansTableUsers($db, "principal", $_POST["adresse"]);
-                    $donneesUtilisateur = getDansTableUsers($db, "nom", $_POST["nom"]);
-                    variablesSession($donneesUtilisateur);
-
-                    $_SESSION['message'] = "Vous êtes bien inscrit";
-                    include("vue/accueil/accueil.php");
-                } else {
-                    $messageErreur = "Ce mail ou nom est déja utilisé";
+                if (preg_match("#^[a-z0-9_.-]+@[a-z0-9_.-]{2,}\.[a-z]{2,4}$#",$_POST['mail'])) {
+                    if (getDansTableUsers($db, "nom", $_POST["nom"]) == array()) { // on verifie que l'array est vide, permet de verifier que "nom" n'existe pas déja dans la table
+                        insertDansTableUsers($db, "principal", $_POST["adresse"]);
+                        $donneesUtilisateur = getDansTableUsers($db, "nom", $_POST["nom"]);
+                        variablesSession($donneesUtilisateur);
+                        $_SESSION['message'] = "Vous êtes bien inscrit";
+                        include("vue/accueil/accueil.php");
+                    } else {
+                        $messageErreur = "Ce mail ou nom est déja utilisé";
+                        include("vue/espaceClient/inscription.php");
+                    }
+                }
+                else {
+                    $messageErreur = "Attention ton adresse mail n'es pas valide";
                     include("vue/espaceClient/inscription.php");
                 }
             } else {
@@ -47,15 +52,21 @@ if ($utilisateurSecondaire == True) {
     if (isset($_POST["nom"]) && isset($_POST["mdp"]) && isset($_POST["rmdp"]) && isset($_POST["mail"]) && $_GET['cible'] == "controllerInscriptionSecondaire") {
         if (!empty($_POST["nom"]) && !empty($_POST["mail"]) && !empty($_POST["mdp"]) && !empty($_POST["rmdp"])) {
             if ($_POST["mdp"] == $_POST["rmdp"]) {
-                if (getDansTableUsers($db, "nom", $_POST["nom"]) == array()) { // on verifie que l'array est vide, permet de verifier que "nom" n'existe pas déja dans la table
-                    insertDansTableUsers($db, "secondaire", $_SESSION["adresse"]);
-                    $donneesUtilisateur = getDansTableUsers($db, "nom", $_POST["nom"]);
+                if (preg_match("#^[a-z0-9_.-]+@[a-z0-9_.-]{2,}\.[a-z]{2,4}$#",$_POST['mail'])) {
+                    if (getDansTableUsers($db, "nom", $_POST["nom"]) == array()) { // on verifie que l'array est vide, permet de verifier que "nom" n'existe pas déja dans la table
+                        insertDansTableUsers($db, "secondaire", $_SESSION["adresse"]);
+                        $donneesUtilisateur = getDansTableUsers($db, "nom", $_POST["nom"]);
 
-                    $messageErreur = "L'Utilisateur secondaire a bien été créer";
-                    include("vue/espaceClient/modifierDonneesPerso.php");
+                        $messageErreur = "L'Utilisateur secondaire a bien été crée";
+                        include("vue/espaceClient/modifierDonneesPerso.php");
 
-                } else {
-                    $messageErreur = "Ce mail ou nom est déja utilisé";
+                    } else {
+                        $messageErreur = "Ce mail ou nom est déja utilisé";
+                        include("vue/espaceClient/inscription.php");
+                    }
+                }
+                else {
+                    $messageErreur = "Attention ton adresse mail n'es pas valide";
                     include("vue/espaceClient/inscription.php");
                 }
             } else {
--- index.php
+++ index.php
@@ -3,3 +3,33 @@ session_start();
 include("modele/connexionDB.php");
 include("controller/redirection.php");
 ?>
+
+
+
+<!--/*
+
+function select($db, $query, $param) {
+    $requete = $db->prepare($query);
+    $requete->execute($param);
+
+    $donnees = $requete->fetch();
+    $requete->closeCursor();
+    return $donnees;
+}
+
+function getDansTableUsers($param,$where)
+{
+    if ($where == "userId") {
+        $query = 'SELECT userID, mail, mdp, nom, adresse,role, dateInscription FROM users WHERE userID=:userID';
+
+        $table = $param;
+
+    }
+    return select($db,$query, ["userID" => $post]);
+}
+
+
+function tableUtilisateur()
+
+
+-->
