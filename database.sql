3fdf96fe6b2631dc3b93f64d13faae2a845c977d
TristanMngr <tristanmenager@gmail.com>
2016-12-12 14:50:25 +0100

    ajout toute les table

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:8889
--- Généré le :  Lun 12 Décembre 2016 à 14:32
+-- Généré le :  Lun 12 Décembre 2016 à 14:49
 -- Version du serveur :  5.6.28
 -- Version de PHP :  5.6.25
 
@@ -28,6 +28,35 @@ CREATE TABLE `salles` (
   `IDuser` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `dateInscription` date NOT NULL,
+  `role` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `userssalles`
+--
+
+CREATE TABLE `userssalles` (
+  `ID` int(11) NOT NULL,
+  `IDuser` int(11) NOT NULL,
+  `IDsalle` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Index pour les tables exportées
 --
@@ -39,6 +68,18 @@ ALTER TABLE `salles`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Index pour la table `userssalles`
+--
+ALTER TABLE `userssalles`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- AUTO_INCREMENT pour les tables exportées
 --
 
@@ -46,4 +87,14 @@ ALTER TABLE `salles`
 -- AUTO_INCREMENT pour la table `salles`
 --
 ALTER TABLE `salles`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `userssalles`
+--
+ALTER TABLE `userssalles`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
