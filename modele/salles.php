fd6bfed7143419ffe91b1c95764eaf43f21ef562
TristanMngr <tristanmenager@gmail.com>
2017-01-02 23:19:01 +0100

    inner join sur table: capteurs/archives/salles

--- modele/salles.php
+++ modele/salles.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tristanmenager
- * Date: 02/01/2017
- * Time: 20:08
- */
+
+function getDataSalle($db,$tableau) {
+    $query = 'SELECT capteurs.type, archives.temperature, archives.humidite, archives.numero 
+    FROM salles
+    JOIN capteurs ON capteurs.ID_salle = salles.ID
+    JOIN archives ON archives.ID_capteur = capteurs.ID
+    WHERE capteurs.ID_salle =:ID_salle';
+
+
+    $param = $tableau['param'] ;
+    $requete = $db->prepare($query);
+    $requete->execute($param);
+
+    $tableau = array();
+    while ($donnees = $requete->fetch()) {
+
+        $tableau[] = $donnees;
+    }
+    return $tableau;
+}
+
+
