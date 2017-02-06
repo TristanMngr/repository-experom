f366ac029f1ccd1f0134ad4d3a5d19bb3142ffde
TristanMngr <tristanmenager@gmail.com>
2017-02-06 22:10:26 +0100

    nom != general

--- controller/espaceClient/maison/createSalleV2.php
+++ controller/espaceClient/maison/createSalleV2.php
@@ -11,48 +11,52 @@ $arraySerialKey = explode(',',$stringSerialKey);
 
 
 if (isset($_POST['nomSalle']) & !empty($_POST['nomSalle'])) {
-    if ($stringSerialKey != "") {
-        $tableau = array('typeDeRequete' => 'select', 'table' => 'salles', 'param' => array('nom' => $_POST['nomSalle'], 'IDmaison' => $_SESSION['IDmaison']));
-        if (requeteDansTable($db, $tableau) == array()) {
-            // récupère le type
-            for ($key = 0; $key < count($arraySerialKey); $key++) {
-                if (typeCapteur(substr($arraySerialKey[$key], 0, 1)) == 'temperature') {
-                    $isTemp = true;
-                }
-                if (typeCapteur(substr($arraySerialKey[$key], 0, 1)) == 'humidite') {
-                    $isHum = true;
-                }
-            }
-            $tableau = array('typeDeRequete' => 'insert', 'table' => 'salles', 'param' => array('nom' => $_POST['nomSalle'], 'isTemperature' => $isTemp, 'isHumidite' => $isHum, 'IDmaison' => $_SESSION['IDmaison']));
-            requeteDansTable($db, $tableau);
-
-            $idSalle = getLastID($db);
-
-            for ($key = 0; $key < count($arraySerialKey); $key++) {
-
-                $tableau = array('ID_salle' => $idSalle, 'serial_key' => (int)$arraySerialKey[$key], 'ID_maison' => $_SESSION['IDmaison']);
-
-                updateTableCapteur($db, $tableau);
-            }
-            //TODO ajout salle general si n'existe pas
-
-            $tableau = array('typeDeRequete' => 'select', 'table' => 'salles', 'param' => array('IDmaison' => $_SESSION['IDmaison'], 'nom' => 'general'));
-
-
+    if ($_POST['nomSalle'] != "general") {
+        if ($stringSerialKey != "") {
+            $tableau = array('typeDeRequete' => 'select', 'table' => 'salles', 'param' => array('nom' => $_POST['nomSalle'], 'IDmaison' => $_SESSION['IDmaison']));
             if (requeteDansTable($db, $tableau) == array()) {
-                $tableau = array('typeDeRequete' => 'insert', 'table' => 'salles', 'param' => array('IDmaison' => $_SESSION['IDmaison'], 'nom' => 'general'));
+                // récupère le type
+                for ($key = 0; $key < count($arraySerialKey); $key++) {
+                    if (typeCapteur(substr($arraySerialKey[$key], 0, 1)) == 'temperature') {
+                        $isTemp = true;
+                    }
+                    if (typeCapteur(substr($arraySerialKey[$key], 0, 1)) == 'humidite') {
+                        $isHum = true;
+                    }
+                }
+                $tableau = array('typeDeRequete' => 'insert', 'table' => 'salles', 'param' => array('nom' => $_POST['nomSalle'], 'isTemperature' => $isTemp, 'isHumidite' => $isHum, 'IDmaison' => $_SESSION['IDmaison']));
                 requeteDansTable($db, $tableau);
-                $showGeneral = true;
+
+                $idSalle = getLastID($db);
+
+                for ($key = 0; $key < count($arraySerialKey); $key++) {
+
+                    $tableau = array('ID_salle' => $idSalle, 'serial_key' => (int)$arraySerialKey[$key], 'ID_maison' => $_SESSION['IDmaison']);
+
+                    updateTableCapteur($db, $tableau);
+                }
+                //TODO ajout salle general si n'existe pas
+
+                $tableau = array('typeDeRequete' => 'select', 'table' => 'salles', 'param' => array('IDmaison' => $_SESSION['IDmaison'], 'nom' => 'general'));
+
+
+                if (requeteDansTable($db, $tableau) == array()) {
+                    $tableau = array('typeDeRequete' => 'insert', 'table' => 'salles', 'param' => array('IDmaison' => $_SESSION['IDmaison'], 'nom' => 'general'));
+                    requeteDansTable($db, $tableau);
+                    $showGeneral = true;
+                }
+
+                $messageSuccess = 'Votre salle a été créée avec succès';
+
+            } else {
+                $messageError = 'Ce nom est déja utilisé';
             }
-
-            $messageSuccess = 'Votre salle a été créée avec succès';
-
         } else {
-            $messageError = 'Ce nom est déja utilisé';
+            $messageError = "Vous n'avez séléctionné aucun capteur";
         }
     }
     else {
-        $messageError = "Vous n'avez séléctionné aucun capteur";
+        $messageError = "Veuillez utiliser un autre nom";
     }
 }
 
